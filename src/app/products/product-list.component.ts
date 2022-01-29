ef8ed209df9e30e678750071cdd60035723e0c4d
André Almeida <andalvalmeida@gmail.com>
2022-01-28 21:17:58 -0300

    Output Decorator

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -45,6 +45,10 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     this.showImage = !this.showImage;
   }
 
+  onValueChange(value: string): void {
+    this.performFilter(value);
+  }
+
   performFilter(filterBy?: string): void {
     if (filterBy) {
       this.filteredProducts = this.products.filter((product: IProduct) =>
--- src/app/shared/criteria/criteria.component.ts
+++ src/app/shared/criteria/criteria.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'pm-criteria',
@@ -6,12 +7,22 @@ import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleC
   styleUrls: ['./criteria.component.css']
 })
 export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
-  listFilter: string = 'cart';
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
   hitMessage: string;
 
+  @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+
   @ViewChild('filterElement') filterElementRef: ElementRef;
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.valueChange.emit(value);
+  }
 
   constructor() { }
 
